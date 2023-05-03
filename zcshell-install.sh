fa065595e7872dbc90416e05fea388962b032c89
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2023-05-03 23:22:40 +0530

    update

--- zcshell-install.sh
+++ zcshell-install.sh
@@ -1,10 +1,11 @@
 #!bin/bash
+
 echo Node Version 
 node -v
 
 echo npm Version
 npm -v
 
-# git clone https://github.com/DebanjanT/zcshell.git zcshelldemo
-# cd zcshelldemo
+git clone https://github.com/DebanjanT/zcshell.git zcshelldemo
+cd zcshelldemo
 
