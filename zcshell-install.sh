d999614c4a8ffe685538070b18ec5d5dad617b0a
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2023-05-14 00:06:11 +0530

    update IS

--- zcshell-install.sh
+++ zcshell-install.sh
@@ -11,16 +11,29 @@ yarn -v
 
 
 git clone https://github.com/DebanjanT/zcshell.git zcshell
+
 cd zcshell
 
+# read user input for server port then set it into a variable
+echo "Enter your server port"
+
+read port
+
 # create .env
-echo "PORT=9293" > .env
+echo "PORT=$port" > .env
+
+# read user input for linux user name then set it into a variable
+echo "Enter your linux user name"
+
+read username
+
+echo "USER_SERVER_DIR=/home/$username/" >> .env
 
 # install dependencies
 yarn install
 
 # run server 
-pm2 start "yarn dev"
+pm2 start "yarn dev" --name zcshell
 
 
 
